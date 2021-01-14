e199b2657edd57c367a5ae7ffe1a7c1cb2eb1c37
Gabriella Bellanca <gabriella1bellanca@gmail.com>
2021-01-14 21:40:55 +0000

    -antibiotic prescribing trends for bacterial chest infections

--- chest_infections_prescribing.py
+++ chest_infections_prescribing.py
@@ -1,22 +1,13 @@
+#Request module retrives data from websites.
 import requests
 import pandas as pd
 
-def run_sql(sql_query):
-    url = "https://opendata.nhsbsa.net/api/3/action/datastore_search_sql?sql=%s" % sql_query
-    data = requests.get(url)
-    records = data.json()['result']['result']['records']
-    return pd.DataFrame(records), data
-
-df, data = run_sql("SELECT * from EPD_202009 limit 1000")
-
-''' Function takes the name of a drug (case insensitive)' and gives bnf codes '''
+''' Defining Function takes the name of a drug (case insensitive)' and gives bnf codes '''
 def get_bnf_codes(drug):
     response = requests.get('https://openprescribing.net/api/1.0/bnf_code/?format=json&q=%s' % drug)
     data = response.json()
     return pd.DataFrame(data)
 
-amoxicillin_codes = get_bnf_codes('amoxicillin')
-print(amoxicillin_codes)
 
 def get_spending_by_month_for_bnf_code(bnf_code):
     response = requests.get('https://openprescribing.net/api/1.0/spending/?format=json&code=%s' % bnf_code)
@@ -29,4 +20,31 @@ def get_ccg_spending_by_month_for_bnf_code(bnf_code):
     data = response.json()
     return pd.DataFrame(data)
 
-get_ccg_spending_by_month_for_bnf_code(amoxicillin_codes.iloc[0]['id'])
+spending_by_bnf_code = {}#create a dictionary
+bnf_code_by_drug_name = {}
+for drug in ["amoxicillin", "doxycycline", "clarithromycin","Erythromycin"]:
+    codes = get_bnf_codes(drug)
+    bnf_code_by_drug_name[drug] = codes
+    for code in codes["id"]:
+        print(code)
+        spending_by_bnf_code[code] = get_spending_by_month_for_bnf_code(code)
+
+items = []
+for drug in bnf_code_by_drug_name.keys():
+    df = bnf_code_by_drug_name[drug]
+    df["generic_drug"] = drug
+    items.append(df)
+drug_lookup = pd.concat(items)
+
+items = []
+for bnf_code in spending_by_bnf_code.keys():
+    df = spending_by_bnf_code[bnf_code]
+    df["bnf_code"] = bnf_code
+    items.append(df)
+total_spending_drug = pd.concat(items)
+
+merged_data = pd.merge(total_spending_drug,drug_lookup,left_on="bnf_code", right_on="id")
+
+merged_data.pivot_table(values="actual_cost",index="date", columns="generic_drug", aggfunc="sum")
+
+merged_data.pivot_table(values="quantity", index= "date", columns="generic_drug", aggfunc="sum")
